da767d1f70c00c48c7c988862ac1ca6e08b24050
Robert McLay <mclay@zaniahgroup.com>
2013-11-29 06:08:28 -0600

    added termwidth, changed bash timer test

---
+++ tools/TermWidth.lua
@@ -0,0 +1,57 @@
+-----------------------------------------------------------------------
+--
+--  Copyright (C) 2008-2013 Robert McLay
+--
+--  Permission is hereby granted, free of charge, to any person obtaining
+--  a copy of this software and associated documentation files (the
+--  "Software"), to deal in the Software without restriction, including
+--  without limitation the rights to use, copy, modify, merge, publish,
+--  distribute, sublicense, and/or sell copies of the Software, and to
+--  permit persons to whom the Software is furnished to do so, subject
+--  to the following conditions:
+--
+--  The above copyright notice and this permission notice shall be
+--  included in all copies or substantial portions of the Software.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+--  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+--  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+--  NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+--  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+--  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+--  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+--  THE SOFTWARE.
+--
+--------------------------------------------------------------------------
+
+--------------------------------------------------------------------------
+-- TermWidth(): Use tput cols to find the number of columns.  Then check
+--              stderr to see if it is connected to a tty.  If not then
+--              use 80 columns wide as default.
+
+require("strict")
+require("capture")
+local capture = capture or function (s) return nil end
+local getenv  = os.getenv
+local term    = false
+local s_width = false
+local s_DFLT  = 80
+if (pcall(require,"term")) then
+   term = require("term")
+end
+
+function TermWidth()
+   if (s_width) then
+      return s_width
+   end
+   s_DFLT  = tonumber(getenv("LMOD_TERM_WIDTH")) or s_DFLT
+   s_width = s_DFLT
+   if (getenv("TERM") and term and term.isatty(io.stderr)) then
+      s_width = tonumber(capture("tput cols")) or s_DFLT
+   end
+
+
+   s_width = (s_width > 30) and s_width or 30
+
+   return s_width
+end
